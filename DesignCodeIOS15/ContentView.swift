ff7c9a016271d430c9bd2b876856ce07609e8469
OptimusPrime <gulliverraed@gmail.com>
2025-05-27 19:16:42 +0300

    Added all Assets Finished section 5 'Shape, Stroke and Blend Mode' in the DesignCode IOS 15 first course.

--- DesignCodeIOS15/ContentView.swift
+++ DesignCodeIOS15/ContentView.swift
@@ -9,13 +9,61 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(alignment: .leading, spacing: 8) {
+            Spacer()
+            
+            Image("Logo 2")
+                .resizable(resizingMode: .stretch)
+                .aspectRatio(contentMode: .fit)
+                .background(.ultraThinMaterial)
+                .frame(width: 26, height: 26)
+                .cornerRadius(20)
+                .padding(.all, 9)
+                .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 20, style: .continuous))
+            
+            Text("SwiftUI for IOS 15")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .foregroundStyle(.linearGradient(colors: [.primary, .primary.opacity(0.5)], startPoint: .topLeading, endPoint: .bottomTrailing))
+            
+            Text("20 Sections - 3 hours".uppercased())
+                .font(.footnote)
+                .fontWeight(.semibold)
+                .foregroundStyle(.secondary)
+                
+            Text("Build an IOS ap for IOS 15 with custom layouts, animations and...")
+                .font(.footnote)
+                .foregroundStyle(.secondary)
+                .multilineTextAlignment(.leading)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .lineLimit(2)
+            
+            
         }
-        .padding()
+        .padding(.all, 20)
+        .padding(.vertical, 20)
+        .frame(height: 350)
+        .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 30, style: .continuous))
+        .shadow(color: Color("Shadow").opacity(0.3), radius: 10, x: 0, y: 10)
+        .overlay {
+            RoundedRectangle(cornerRadius: 30, style: .continuous)
+                .stroke(.linearGradient(colors: [.white.opacity(0.3), .black.opacity(0.1)], startPoint: .topLeading, endPoint: .bottomTrailing))
+                .blendMode(.overlay)
+        }
+        .padding(.horizontal, 20)
+        .background(
+            Image("Blob 1")
+                .offset(x: 250, y: -100)
+        )
+        .overlay(
+            Image("Illustration 5")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(height: 230)
+                .offset(x: 32, y: -80)
+        )
+        
+        
     }
 }
 
