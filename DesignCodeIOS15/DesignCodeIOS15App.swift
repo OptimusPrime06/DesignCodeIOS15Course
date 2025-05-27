3c7b82f0a6c0fb7b23bbb2c5a36214deb7663104
OptimusPrime <gulliverraed@gmail.com>
2025-05-27 16:06:04 +0300

    Initial Commit

---
+++ DesignCodeIOS15/DesignCodeIOS15App.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeIOS15App.swift
+//  DesignCodeIOS15
+//
+//  Created by Gulliver Raed on 5/27/25.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeIOS15App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
